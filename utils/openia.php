bad50f9b2ed3c54a3544ff13b0b4791214f1bb5a
Yannis Piot Pilot <yannis@piotpilot.eu>
2024-02-04 23:00:45 +0100

    Refacto prompt

--- utils/openia.php
+++ utils/openia.php
@@ -2,13 +2,12 @@
 
 function generatePrompt($stock_list)
 {
-    $start = "Salut, j'aimerais faire plusieurs recettes pour ma semaine et j'ai actuellement : ";
-    $end = " que me conseilles-tu comme recette ?";
+    $start = "Peut tu me faire des recettes pour 2 personnes avec les ingrédients suivant : ";
     $list = array_map(function($value) {
         $product_unit_id = $value->product->product_group_id;
         $product_details = getProductDetails($product_unit_id)->quantity_unit_stock->name;
         return $value->amount . " " . $product_details . " de " . $value->product->name;
     }, $stock_list);
     
-    return $start . implode(", ", $list) . $end;
+    return $start . implode(", ", $list);
 }
